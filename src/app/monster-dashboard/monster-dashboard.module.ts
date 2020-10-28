a9402f1bf548f1ae7c88d872b33141cb64a1923f
Marcel Pohland <m.pohland@mwaysolutions.com>
2020-10-28 12:20:50 +0100

    add keplr integration

--- src/app/monster-dashboard/monster-dashboard.module.ts
+++ src/app/monster-dashboard/monster-dashboard.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MonsterRoutingModule } from './monster-dashboard-routing.module';
+import { TransferService } from './services/transfer.service';
+import { MonsterDashboardComponent } from './pages/monster-dashboard/monster-dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MonsterDashboardComponent],
   imports: [
     CommonModule,
     MonsterRoutingModule
-  ]
+  ],
+  exports: [MonsterDashboardComponent],
+  providers: [TransferService]
 })
 export class MonsterDashboardModule { }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,7 +51,10 @@
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
-
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
