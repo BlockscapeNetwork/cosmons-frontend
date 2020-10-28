85c7c40e4d6a697b9ce529a6d4179a80fe01fcd9
Marcel Pohland <m.pohland@mwaysolutions.com>
2020-10-28 18:25:20 +0100

    comment

--- src/app/monster-dashboard/pages/monster-dashboard/monster-dashboard.component.ts
+++ src/app/monster-dashboard/pages/monster-dashboard/monster-dashboard.component.ts
@@ -8,16 +8,26 @@ import { TransferService, ExBankBalancesResponse } from '../../services/transfer
 })
 export class MonsterDashboardComponent implements OnInit {
   acc: ExBankBalancesResponse;
+  monsterAddr: [];
   constructor(private transferService: TransferService) {
   }
 
   ngOnInit(): void {
+    // Get AccountDetails
     this.transferService.getAccount().then((value) => { this.acc = value });
-    this.transferService.queryMonster('cosmos1pgvk0pzmmrz5syz3dxxfav39pe8h5unxdrx5e0', 'monster112a9lf95atqvyejqe22xnna8x4mfqd75tkq2kvwcjyysarcsb').then((value) => { console.log(JSON.stringify(value)) });
-    this.transferService.queryMonster('cosmos1pgvk0pzmmrz5syz3dxxfav39pe8h5unxdrx5e0', 'monster112a9lf95atqvyejqe22xnna8x4mfqd75tkq2kvwcjyysarcsx').then((value) => { console.log(JSON.stringify(value)) });
-    this.transferService.queryAllMonsterInfo('cosmos1pgvk0pzmmrz5syz3dxxfav39pe8h5unxdrx5e0', 'monster112a9lf95atqvyejqe22xnna8x4mfqd75tkq2kvwcjyysarcsb').then((value) => { console.log(JSON.stringify(value)) });
+    // Fetch all possible contract Token/Monster
+    this.transferService.queryAllMonsterAddr('cosmos1pgvk0pzmmrz5syz3dxxfav39pe8h5unxdrx5e0').then((value) => {
+      this.monsterAddr = value.tokens;
+      if (this.monsterAddr.length !== 0) {
+        // Fetch more details for every Monster
+        this.monsterAddr.forEach(element => {
+          this.transferService.queryAllMonsterInfo('cosmos1pgvk0pzmmrz5syz3dxxfav39pe8h5unxdrx5e0', element).then((value) => { console.log(JSON.stringify(value)) });
+        });
+      }
+    });
+
+    // Fetch number of Tokens
     this.transferService.queryNumOfMonster('cosmos1pgvk0pzmmrz5syz3dxxfav39pe8h5unxdrx5e0').then((value) => { console.log(JSON.stringify(value)) });
-    this.transferService.queryAllMonsterAddr('cosmos1pgvk0pzmmrz5syz3dxxfav39pe8h5unxdrx5e0').then((value) => { console.log(JSON.stringify(value)) });
   }
 
 }
