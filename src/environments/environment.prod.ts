3af5f2b451692267cca1fd939b352993bbfb4182
Marcel Pohland <m.pohland@mwaysolutions.com>
2020-11-02 15:41:24 +0100

    set to working contracts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  contractAddress721: "cosmos15wwqx340fed8z9etgwerryvd8z9kqca5kk60zw",
+  contractAddress721: "cosmos1g9vtch4xc9pl0fq2586mapzyjaw2yvv9xlcs6j",
   contractAddress20: "cosmos1kfz3mj84atqjld0ge9eccujvqqkqdr4qqs9ud7",
-  contractAddressMarket: "cosmos1hjh34wfmuplcs5f69f8skfskhum7ekcd0qwh9a",
+  contractAddressMarket: "cosmos1knqr4zclds5zhn5khkpexkd7nctwe8z0s2qer4",
   lightClient: "https://hackatom-lightclient.blockscape.network"
 };
