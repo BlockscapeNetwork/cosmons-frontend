82952a52604f674ed5bb3ca6aedf2302fc15dd3f
Marcel Pohland <m.pohland@mwaysolutions.com>
2020-11-02 18:58:18 +0100

    change cosmons contract

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  contractAddress721: "cosmos1g9vtch4xc9pl0fq2586mapzyjaw2yvv9xlcs6j",
+  contractAddress721: "cosmos1mmsjc833wwqycclmvy5020dj4vwjj8qtnn8ss8",
   contractAddress20: "cosmos1kfz3mj84atqjld0ge9eccujvqqkqdr4qqs9ud7",
   contractAddressMarket: "cosmos1knqr4zclds5zhn5khkpexkd7nctwe8z0s2qer4",
   lightClient: "https://hackatom-lightclient.blockscape.network"
